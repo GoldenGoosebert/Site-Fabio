c1b3259c01b3e37ba82c03611ed6ea3634801996
Florin <ungureanuandreiflorin234@gmail.com>
2024-09-17 21:00:13 +0300

    Contact part basically done

--- public/scripts/navbar-scroll.js
+++ public/scripts/navbar-scroll.js
@@ -4,15 +4,6 @@ document.addEventListener("scroll", function() {
 
   let currentSectionId = "";
 
-  sections.forEach((section) => {
-    const sectionTop = section.offsetTop;
-    const sectionHeight = section.clientHeight;
-
-    if (window.pageYOffset >= sectionTop - sectionHeight / 3) {
-      currentSectionId = section.getAttribute("id");
-    }
-  });
-
   navLinks.forEach((link) => {
     link.classList.remove("active");
     if (link.getAttribute("href").substring(1) === currentSectionId) {
